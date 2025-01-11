9137b9cdfa5fed30694eaf957e1040209599def9
Aryan Sharma <aryansh0004@gmail.com>
2025-01-11 20:29:09 +0530

    Add Cryto Dashboard and lucide react package

--- cryptotask/app/page.tsx
+++ cryptotask/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import CryptoDashboard from './[coinId]/CryptoDashboard'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-start justify-start">
       <Navbar />
+      <CryptoDashboard initialCoinId="bitcoin" />
     </div>
   );
 }
